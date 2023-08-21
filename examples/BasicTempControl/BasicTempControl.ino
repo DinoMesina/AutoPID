e6172e15fcb5347e0a03bdf50737e18ca230d9eb
Dino del Favero <dino@mesina.net>
2023-08-21 16:22:09 +0200

    Add files via upload

--- examples/BasicTempControl/BasicTempControl.ino
+++ examples/BasicTempControl/BasicTempControl.ino
@@ -1,22 +1,22 @@
 /*
-   AutoPID BasicTempControl Example Sketch
+   AutoPID_V2 BasicTempControl Example Sketch
 
    This program reads a dallas temperature probe as input, potentiometer as setpoint, drives an analog output.
    It lights an LED when the temperature has reached the setpoint.
 */
-#include <AutoPID.h>
+#include <AutoPID_V2.h>
 #include <DallasTemperature.h>
 #include <OneWire.h>
 
-//pins
+// pins
 #define POT_PIN A0
 #define OUTPUT_PIN A1
 #define TEMP_PROBE_PIN 5
 #define LED_PIN 6
 
-#define TEMP_READ_DELAY 800 //can only read digital temp sensor every ~750ms
+#define TEMP_READ_DELAY 900 // can only read digital temp sensor every ~750ms
 
-//pid settings and gains
+// PID_V2 settings and gains
 #define OUTPUT_MIN 0
 #define OUTPUT_MAX 255
 #define KP .12
@@ -28,22 +28,22 @@ double temperature, setPoint, outputVal;
 OneWire oneWire(TEMP_PROBE_PIN);
 DallasTemperature temperatureSensors(&oneWire);
 
-//input/output variables passed by reference, so they are updated automatically
-AutoPID myPID(&temperature, &setPoint, &outputVal, OUTPUT_MIN, OUTPUT_MAX, KP, KI, KD);
+// input/output variables passed by reference, so they are updated automatically
+AutoPID_V2 myPID_V2(&temperature, &setPoint, &outputVal, OUTPUT_MIN, OUTPUT_MAX, KP, KI, KD);
 
-unsigned long lastTempUpdate; //tracks clock time of last temp update
+unsigned long lastTempUpdate; // tracks clock time of last temp update
 
-//call repeatedly in loop, only updates after a certain time interval
-//returns true if update happened
+// call repeatedly in loop, only updates after a certain time interval
+// returns true if update happened
 bool updateTemperature() {
   if ((millis() - lastTempUpdate) > TEMP_READ_DELAY) {
-    temperature = temperatureSensors.getTempFByIndex(0); //get temp reading
+    temperature = temperatureSensors.getTempFByIndex(0); // get temp reading
     lastTempUpdate = millis();
-    temperatureSensors.requestTemperatures(); //request reading for next time
+    temperatureSensors.requestTemperatures(); // request reading for next time
     return true;
   }
   return false;
-}//void updateTemperature
+} // void updateTemperature
 
 
 void setup() {
@@ -53,21 +53,22 @@ void setup() {
 
   temperatureSensors.begin();
   temperatureSensors.requestTemperatures();
-  while (!updateTemperature()) {} //wait until temp sensor updated
+  while (!updateTemperature()) {} // wait until temp sensor updated
+  temperatureSensors.setWaitForConversion(false);  // makes it async
 
-  //if temperature is more than 4 degrees below or above setpoint, OUTPUT will be set to min or max respectively
-  myPID.setBangBang(4);
-  //set PID update interval to 4000ms
-  myPID.setTimeStep(4000);
+  // if temperature is more than 4 degrees below or above setpoint, OUTPUT will be set to min or max respectively
+  myPID_V2.setBangBang(4);
+  // set PID_V2 update interval to 4000ms
+  myPID_V2.setTimeStep(4000);
 
-}//void setup
+} // void setup
 
 
 void loop() {
   updateTemperature();
   setPoint = analogRead(POT_PIN);
-  myPID.run(); //call every loop, updates automatically at certain time interval
+  myPID_V2.run(); // call every loop, updates automatically at certain time interval
   analogWrite(OUTPUT_PIN, outputVal);
-  digitalWrite(LED_PIN, myPID.atSetPoint(1)); //light up LED when we're at setpoint +-1 degree
+  digitalWrite(LED_PIN, myPID_V2.atSetPoint(1)); // light up LED when we're at setpoint +-1 degree
 
-}//void loop
+} // void loop
