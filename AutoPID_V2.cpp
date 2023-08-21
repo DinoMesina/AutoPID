d5c7414358f90e2903edcd9baf0f6526fd30be74
Dino del Favero <dino@mesina.net>
2023-08-21 15:33:52 +0200

    Add files via upload

---
+++ AutoPID_V2.cpp
@@ -0,0 +1,104 @@
+#include "AutoPID_V2.h"
+
+AutoPID_V2::AutoPID_V2(double *input, double *setpoint, double *output, double outputMin, double outputMax,
+                 double Kp, double Ki, double Kd) {
+  _input = input;
+  _setpoint = setpoint;
+  _output = output;
+  _outputMin = outputMin;
+  _outputMax = outputMax;
+  setGains(Kp, Ki, Kd);
+  _timeStep = 1000;
+} // AutoPID_V2::AutoPID_V2
+
+void AutoPID_V2::setGains(double Kp, double Ki, double Kd) {
+  _Kp = Kp;
+  _Ki = Ki;
+  _Kd = Kd;
+} // AutoPID_V2::setControllerParams
+
+void AutoPID_V2::setBangBang(double bangOn, double bangOff) {
+  _bangOn = bangOn;
+  _bangOff = bangOff;
+} // void AutoPID_V2::setBangBang
+
+void AutoPID_V2::setBangBang(double bangRange) {
+  setBangBang(bangRange, bangRange);
+} // void AutoPID_V2::setBangBang
+
+void AutoPID_V2::setOutputRange(double outputMin, double outputMax) {
+  _outputMin = outputMin;
+  _outputMax = outputMax;
+} // void AutoPID_V2::setOutputRange
+
+void AutoPID_V2::setTimeStep(unsigned long timeStep){
+  _timeStep = timeStep;
+}
+
+
+bool AutoPID_V2::atSetPoint(double threshold) {
+  return abs(*_setpoint - *_input) <= threshold;
+} // bool AutoPID_V2::atSetPoint
+
+void AutoPID_V2::run() {
+  if (_stopped) {
+    _stopped = false;
+    reset();
+  }
+  // if bang thresholds are defined and we're outside of them, use bang-bang control
+  if (_bangOn && ((*_setpoint - *_input) > _bangOn)) {
+    *_output = _outputMax;
+    _lastStep = millis();
+  } else if (_bangOff && ((*_input - *_setpoint) > _bangOff)) {
+    *_output = _outputMin;
+    _lastStep = millis();
+  } else {                                                // otherwise use PID control
+    unsigned long _dT = millis() - _lastStep;             // calculate time since last update
+    if (_dT >= _timeStep) {                               // if long enough, do PID calculations
+      _lastStep = millis();
+      _dT /= 1000.0;                                      // delta time in seconds 
+      double _error = *_setpoint - *_input;               // error
+      double _deriv = (*_input - _previousInput) / _dT;   // derivative of input, it is not influenced by the setpoint changes  
+      double PD = (_error * _Kp) - (_deriv * _Kd);        // PD (-D because is referenced to the input values)
+      _integral += ((_error + _previousError) / 2) * _dT; // Riemann sum integral
+      _integral = constrain(_integral, ((_outputMin - PD) / _Ki), ((_outputMax - PD) / _Ki)); // limit the integration sum 
+      double I = _integral * _Ki;                         // I
+      _previousError = _error;
+      _previousInput = *_input;
+      *_output = PD + I;
+    }
+  }
+} // void AutoPID_V2::run
+
+void AutoPID_V2::stop() {
+  _stopped = true;
+  reset();
+}
+void AutoPID_V2::reset() {
+  _lastStep = millis();
+  _integral = 0.0;
+  _previousError = 0.0;
+}
+
+bool AutoPID_V2::isStopped(){
+  return _stopped;
+}
+
+double AutoPID_V2::getIntegral(){
+  return _integral;
+}
+
+void AutoPID_V2::setIntegral(double integral){
+  _integral = integral;
+}
+
+void AutoPID_V2Relay::run() {
+  AutoPID_V2::run();
+  while ((millis() - _lastPulseTime) > _pulseWidth) _lastPulseTime += _pulseWidth;
+  *_relayState = ((millis() - _lastPulseTime) < (_pulseValue * _pulseWidth));
+}
+
+double AutoPID_V2Relay::getPulseValue(){
+  return (isStopped()?0:_pulseValue);
+}
+
---
+++ AutoPID_V2.h
@@ -0,0 +1,64 @@
+#ifndef AUTOPID_V2_V2_H
+#define AUTOPID_V2_V2_H
+#include <Arduino.h>
+
+class AutoPID_V2 {
+
+  public:
+    // Constructor - takes pointer inputs for control variales, so they are updated automatically
+    AutoPID_V2(double *input, double *setpoint, double *output, double outputMin, double outputMax,
+            double Kp, double Ki, double Kd);
+    // Allows manual adjustment of gains
+    void setGains(double Kp, double Ki, double Kd);
+    // Sets bang-bang control ranges, separate upper and lower offsets, zero for off
+    void setBangBang(double bangOn, double bangOff);
+    // Sets bang-bang control range +-single offset
+    void setBangBang(double bangRange);
+    // Allows manual readjustment of output range
+    void setOutputRange(double outputMin, double outputMax);
+    // Allows manual adjustment of time step (default 1000ms)
+    void setTimeStep(unsigned long timeStep);
+    // Returns true when at set point (+-threshold)
+    bool atSetPoint(double threshold);
+    // Runs PID_V2 calculations when needed. Should be called repeatedly in loop.
+    // Automatically reads input and sets output via pointers
+    void run();
+    // Stops PID_V2 functionality, output sets to 
+    void stop();
+    void reset();
+    bool isStopped();
+
+    double getIntegral();
+    void setIntegral(double integral);
+
+  private:
+    double _Kp, _Ki, _Kd;
+    double _integral = 0.0, _previousError = 0.0, _previousInput = 0.0;
+    double _bangOn = 0.0, _bangOff = 0.0;
+    double *_input, *_setpoint, *_output;
+    double _outputMin, _outputMax;
+    unsigned long _timeStep = 0.0, _lastStep = 0.0;
+    bool _stopped = false;
+
+}; // class AutoPID_V2
+
+class AutoPID_V2Relay : public AutoPID_V2 {
+  public:
+
+    AutoPID_V2Relay(double *input, double *setpoint, bool *relayState, double pulseWidth, double Kp, double Ki, double Kd)
+      : AutoPID_V2(input, setpoint, &_pulseValue, 0, 1.0, Kp, Ki, Kd) {
+      _relayState = relayState;
+      _pulseWidth = pulseWidth;
+    };
+
+    void run();
+
+    double getPulseValue();
+
+  private:
+    bool * _relayState;
+    unsigned long _pulseWidth, _lastPulseTime;
+    double _pulseValue;
+}; // class AutoPID_V2Relay
+
+#endif
